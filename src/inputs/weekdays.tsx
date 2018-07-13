fd6dc7321ec4d1ad22b588adf7a03c600200ceed
esepato <markus_patoka@hotmail.com>
2018-07-13 11:54:12 +0200

    - Updated version - Added weekdays

---
+++ src/inputs/weekdays.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+type ButtonProps = {
+  checked?: boolean
+}
+
+export const Days = styled.div`
+  background-color: transparent;
+  min-width: 300px;
+  display: flex;
+  justify-content: space-between;
+  padding: 3px 0;
+  border-radius: 3px;
+`
+
+export const Weekday = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  font-size: 14px;
+  width: 30px;
+  background-color: ${({ checked }: ButtonProps) =>
+    checked
+      ? 'var(--field-alternate-background-color)'
+      : 'var(--field-background-color)'};
+  color: ${({ checked }: ButtonProps) =>
+    checked ? 'var(--toolbar-background-color)' : 'var(--field-text-color)'};
+  border-radius: var(--border-radius);
+  border: none;
+`
