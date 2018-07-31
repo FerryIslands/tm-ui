660a7bdb3f1dee7eb262377f8782b0bdee5a90e2
esepato <markus_patoka@hotmail.com>
2018-07-31 10:38:22 +0200

    - Updated Button component

--- src/button.tsx
+++ src/button.tsx
@@ -1,42 +1,87 @@
+import * as React from 'react'
 import styled from 'styled-components'
+import Spinner from './spinner'
 
 type Props = {
   disabled?: boolean
+  onClick?: () => void
+  icon?: JSX.Element
+  iconPosition?: 'left' | 'right'
+  isLoading?: boolean
   secondary?: boolean
+  text?: string
 }
 
-export default styled.div`
+const Button = styled.button`
   align-items: center;
   background: ${({ secondary }: Props) =>
-    secondary ? '#B1BBC0' : 'var(--accent-color)'};
+    secondary ? '#7F8D9D' : 'var(--accent)'};
   border: 0;
   border-radius: var(--border-radius);
   color: #fff;
-  display: grid;
+  display: flex;
   fill: #fff;
   font-size: 16px;
   font-weight: 700;
-  grid-gap: 10px;
-  grid-auto-flow: column;
-  padding: 10px 12px;
+  height: 34px;
+  padding: 0 12px;
 
   ${({ disabled }: Props) =>
     disabled
       ? `
-background: var(--accent-disabled-color);
+background: #cccdcf;
 color: #8c9196;
 fill: #8c9196;`
       : `
 cursor: pointer;
+
 &:hover {
-  background: var(--accent-hover-color);
+  opacity: 0.9;
 }
+
 &:active {
-  background: var(--accent-active-color);
-  padding: 11px 11px 9px 13px;
+  opacity: 0.7;
+  padding: 1px 11px 0 13px;
 }
 
 `} svg {
     height: 16px;
   }
+
+  svg:first-child:not(:last-child) {
+    margin-right: 8px;
+  }
+
+  svg:last-child:not(:first-child) {
+    margin-left: 8px;
+  }
+
+  ${({ isLoading }: Props) =>
+    isLoading
+      ? `
+  background: #2D284D;
+  color: #8c9196;
+  cursor: default;
+  svg {
+      height: 24px;
+    }
+  `
+      : ``};
 `
+
+export default ({
+  disabled,
+  icon,
+  iconPosition,
+  isLoading,
+  onClick,
+  text,
+}: Props) => (
+  <Button disabled={disabled} onClick={onClick} isLoading={isLoading}>
+    {iconPosition === 'right' && <Text>{text}</Text>}
+    {isLoading ? <Spinner width={24} color={'#fff'} /> : icon}
+    {iconPosition === 'left' && <Text>{text}</Text>}
+  </Button>
+)
+
+const Text = styled.div``
