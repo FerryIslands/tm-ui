f0033590fc7fef56bd48ed9afaf708f95c68b356
esepato <markus_patoka@hotmail.com>
2018-10-02 10:39:34 +0200

    - Changed name of prop

--- src/inputs/select.tsx
+++ src/inputs/select.tsx
@@ -22,7 +22,7 @@ export default (props: any) => (
         borderWidth: 0,
         boxShadow: 'none',
         minHeight: 'auto',
-        minWidth: props.minWidth || 'auto',
+        minWidth: props.controlMinWidth || 'auto',
       }),
       dropdownIndicator: (base: any) => ({
         ...base,
